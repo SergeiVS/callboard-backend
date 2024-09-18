86cb4c4356bd8c053cd88348f5c5a058da610094
SergeiVS <fam.smirnoff@gmail.com>
2024-09-18 20:39:37 +0200

    initial commit

---
+++ src/main/java/org/callboard/CallboardApplication.java
@@ -0,0 +1,13 @@
+package org.callboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/callboard/CallboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.callboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
