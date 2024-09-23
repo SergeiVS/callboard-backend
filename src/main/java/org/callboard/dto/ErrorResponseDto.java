88c4ab45f568b5f60b0680ed70a9e5c72414cf05
SergeiVS <fam.smirnoff@gmail.com>
2024-09-23 21:40:57 +0200

    Add SecurityConfig

---
+++ src/main/java/org/callboard/dto/ErrorResponseDto.java
@@ -0,0 +1,15 @@
+package org.callboard.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ErrorResponseDto {
+    @Schema(description = "ErrorMessage", defaultValue = "Internal Server Error")
+    private String errorMessage;
+
+    @Schema(description = "UserNotFound")
+    private String errorType;
+}
---
+++ src/main/java/org/callboard/exceptions/NotFoundException.java
@@ -0,0 +1,5 @@
+package org.callboard.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){super(message);}
+}
