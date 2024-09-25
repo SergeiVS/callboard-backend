31b6e687aa4b150480457d1ea47a25b19222e5cc
Aleksandr-Yashnik <jashnik.aleksandr@gmail.com>
2024-09-25 11:55:07 +0200

    Add  Entiti post, Subject. Repositaruty

--- src/main/java/org/callboard/entities/Subject.java
+++ src/main/java/org/callboard/entities/Subject.java
@@ -1,15 +1,16 @@
 package org.callboard.entities;
 
-
-import lombok.Data;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import java.io.Serializable;
+import lombok.Data;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "subjects")
 @Data
-public class Subject implements Serializable {
+public class Subject {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +20,28 @@ public class Subject implements Serializable {
     @NotBlank
     @Column(name = "name", nullable = false)
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+
+        if (!thisEffectiveClass.equals(oEffectiveClass)) return false;
+
+        Subject subject = (Subject) o;
+        return subjectId != null && Objects.equals(subjectId, subject.subjectId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subjectId != null ? subjectId : 0);
+    }
 }
