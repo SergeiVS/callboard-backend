64e4449d6edcd4eb4e2ca6eaedd6a8580e32c55d
SergeiVS <fam.smirnoff@gmail.com>
2024-10-08 20:58:05 +0200

    add AuthResponse, JwtProvider

--- src/main/java/org/callboard/security/JwtProvider.java
+++ src/main/java/org/callboard/security/JwtProvider.java
@@ -2,24 +2,20 @@ package org.callboard.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.callboard.entities.User;
-import org.eclipse.angus.mail.util.BASE64DecoderStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
-import java.security.SignatureException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -32,7 +28,7 @@ public class JwtProvider {
     @Value("300000")
     private  long expireAt;
 
-   public String getJwtSecret(User user) {
+   public String generateJwtSecret(User user) {
        final LocalDateTime now = LocalDateTime.now();
        final Instant accessExpirationInstant = now.plusMinutes(5).atZone(ZoneId.systemDefault()).toInstant();
        final Date accessExpiration = Date.from(accessExpirationInstant);
@@ -40,20 +36,14 @@ public class JwtProvider {
                .setSubject(user.getEmail())
                .setExpiration(accessExpiration)
                .signWith(jwtAccessSecret)
-               .setClaims()
-               .claim("roles", user.getRoles().stream().map(role -> STR."ROLE_\{role}"))
+               .claim("roles", getRoles(user))
                .compact();
 
    }
 
 
-
-    public boolean validateToken(@NotBlank String token) {
-        return validateToken(token, jwtAccessSecret);
-    }
-
-    private Claims getClaimsFromUser(){
-       return
+    private List<String> getRoles(User user) {
+       return user.getRoles().stream().map(role -> "Role_"+ role.getRoleName()).toList();
     }
 
     private boolean validateToken(@NotNull String token, @NotNull Key secret) {
