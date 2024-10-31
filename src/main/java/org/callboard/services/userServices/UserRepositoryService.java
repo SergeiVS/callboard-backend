5394da3d0778d9fafc6b124296e8f9ab6bf45cc0
SergeiVS <fam.smirnoff@gmail.com>
2024-10-31 19:34:40 +0100

    fix String

--- src/main/java/org/callboard/services/userServices/UserRepositoryService.java
+++ src/main/java/org/callboard/services/userServices/UserRepositoryService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserRepositoryService {
+
     private final UserRepository userRepository;
 
     public User saveUser(User user) {
